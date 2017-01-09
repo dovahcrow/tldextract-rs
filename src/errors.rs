ca547c515eb80781e196d31765b16952d0014189
Wu Young <doomsplayer@gmail.com>
2017-01-10 01:23:38 +0800

    update error-chain and pump version

--- src/errors.rs
+++ src/errors.rs
@@ -2,19 +2,19 @@ error_chain!{
     types {}
     links {}
     foreign_links {
-         ::serde_json::Error, JsonError;
-         ::hyper::Error, HttpError;
-         ::url::ParseError, UrlParseError;
-         ::std::io::Error, IoError;
+        JsonError(::serde_json::Error);
+        HttpError(::hyper::Error);
+        UrlParseError(::url::ParseError);
+        IoError(::std::io::Error);
     }
     errors {
-         NoHostError(t: String) {
-             description("no host error")
-             display("no such host: '{}'", t)
-         }
-          EmptyDomainError {
-             description("domain should not be empty")
-             display("domain should not be empty")
-         }
+        NoHostError(t: String) {
+            description("no host error")
+            display("no such host: '{}'", t)
+        }
+        EmptyDomainError {
+            description("domain should not be empty")
+            display("domain should not be empty")
+        }
     }
 }
